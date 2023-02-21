0abc8c526a62dc28a8e972ad92a7b6f9f7f797f2
BartoszChadrys <bartekch322@gmail.com>
2023-02-21 19:11:55 +0100

    Initial Commit

---
+++ DzienWydzialu-iOS/Controller/WelcomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DzienWydzialu-iOS
+//
+//  Created by Bartek Chadryś on 17/02/2023.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+

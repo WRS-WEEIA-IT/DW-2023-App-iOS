2fe103e546efac3244873d3cfc27f9bc8a4d5f41
BartoszChadrys <bartekch322@gmail.com>
2023-03-04 21:05:25 +0100

    Scanner design completed

--- DzienWydzialu-iOS/Controller/QrViewController.swift
+++ DzienWydzialu-iOS/Controller/QrViewController.swift
@@ -13,6 +13,8 @@ class QrViewController: UIViewController {
     var avCaptureSession: AVCaptureSession!
     var avPreviewLayer: AVCaptureVideoPreviewLayer!
 
+    @IBOutlet weak var squareFrame: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         avCaptureSession = AVCaptureSession()
@@ -48,11 +50,56 @@ class QrViewController: UIViewController {
                 self.failed()
                 return
             }
-            
+                                                
             self.avPreviewLayer = AVCaptureVideoPreviewLayer(session: self.avCaptureSession)
             self.avPreviewLayer.frame = self.view.layer.bounds
             self.avPreviewLayer.videoGravity = .resizeAspectFill
             self.view.layer.addSublayer(self.avPreviewLayer)
+
+            let frameSize = CGSize(width: 200, height: 200)
+            UIGraphicsBeginImageContextWithOptions(frameSize, false, 0)
+
+            let context = UIGraphicsGetCurrentContext()
+
+            context?.setStrokeColor(UIColor.white.cgColor)
+            context?.setLineWidth(5.0)
+
+            let roundedRectPath = UIBezierPath(roundedRect: CGRect(origin: CGPoint.zero, size: frameSize), cornerRadius: 20.0)
+            roundedRectPath.stroke()
+            let frameImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            
+            let layer = CALayer()
+            
+            let imageView =  frameImage
+            layer.contents = imageView?.cgImage
+            layer.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+            layer.position = CGPoint(x: self.view.layer.bounds.midX, y: self.view.layer.bounds.midY)
+            
+            self.view.layer.addSublayer(layer)
+
+            // Create a text layer
+            let textLayer = CATextLayer()
+
+            // Set the text layer's font
+            textLayer.font = UIFont.systemFont(ofSize: 15).fontName as CFTypeRef
+
+            // Set the text layer's font size
+            textLayer.fontSize = 15.0
+
+            // Set the text layer's text
+            textLayer.string = "Swipe down to exit scanner!"
+            textLayer.contentsScale = UIScreen.main.scale
+            
+            // Calculate the size of the text
+            let textSize = (textLayer.string as? NSString)?.size(withAttributes: [.font: UIFont.systemFont(ofSize: 15)])
+
+            // Center the text layer horizontally
+            textLayer.frame = CGRect(x: ((self.view.layer.bounds.width - textSize!.width) / 2.0), y: 15, width: textSize!.width, height: textSize!.height)
+
+            // Add the text layer to the sublayer
+            self.view.layer.addSublayer(textLayer)
+            
             self.avCaptureSession.startRunning()
         }
     }
